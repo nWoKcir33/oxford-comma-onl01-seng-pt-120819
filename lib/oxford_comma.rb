a4f2fa973e48ead685303abcc73601183b0a6dd4
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-17 03:01:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
-array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+array = ["kiwi"]
 if array [0]
 array.join
+elsif array [0,1]
+array.join(" and ")
 end
 end
