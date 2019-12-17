8e06868987e0f12c4399357cc28f35976927a3fe
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-17 03:10:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
-if array [0]
-puts "#{array [0]}"
-elsif array [0..1]
-array.join(" and ")
+return array.first if array.size == 1
+
+   if array.size == 2
+    array.insert(1, "and").join(" ")
+  elsif array.size > 2
+    array[-1].insert(0, "and ")
+    array.join(", ")
 end
 end
